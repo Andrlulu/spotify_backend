e004d07d2f6a2947eeae67a23b1e7423593a25c2
andrlulu <weijianguo0303@gmail.com>
2025-04-11 12:49:11 -0400

    initialized with provided frame work and added resource

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,28 @@
+val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val kotlin_serialization: String by project
 
 plugins {
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") version "1.8.10"
     id("io.ktor.plugin") version "2.2.4"
+    kotlin("plugin.serialization") version "1.8.10"
 }
 
-group = "laioffer.com"
+group = "com.laioffer"
 version = "0.0.1"
-
 application {
-    mainClass = "laioffer.com.ApplicationKt"
+    mainClass.set("com.laioffer.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+        jvmToolchain(11)
+    }
 }
 
 repositories {
@@ -21,10 +30,12 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlin_serialization")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-core")
-    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/com/laioffer/Application.kt
+++ src/main/kotlin/com/laioffer/Application.kt
@@ -1,8 +1,13 @@
-package laioffer.com.com.laioffer
+package com.laioffer
 
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -10,5 +15,15 @@ fun main() {
 }
 
 fun Application.module() {
-    configureRouting()
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+        })
+    }
+    // TODO: adding the routing configuration here
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
 }
