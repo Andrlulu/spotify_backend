6e673fee4645863e713b470d99c86ecf5fbe9bb7
andrlulu <weijianguo0303@gmail.com>
2025-04-11 12:25:31 -0400

    bare frame, start from fresh

---
+++ src/main/kotlin/com/laioffer/Routing.kt
@@ -0,0 +1,13 @@
+package laioffer.com.com.laioffer
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
